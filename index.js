33ce074eff45e2f66cd2e6f2626199809bff2754
Ayesha Narve <narveayesha@gmail.com>
2025-10-18 12:45:06 +0530

    adding signup pages

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+
+const app = express();
+
+// EJS setup
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Sample routes
+app.get('/', (req, res) => {
+    res.render('home'); // make sure views/home.ejs exists
+});
+
+app.get('/login', (req, res) => {
+    res.render('users/login'); // make sure views/login.ejs exists
+});
+
+
+//student signup route
+app.get('/ssignup', (req, res) => {
+    res.render('users/stud_signup'); // make sure views/login.ejs exists
+});
+
+//guest lecturer signup route
+app.get('/lsignup', (req, res) => {
+    res.render('users/lec_signup'); 
+});
+
+//institute signup route
+app.get('/isignup', (req, res) => {
+    res.render('users/inst_signup'); 
+});
+
+// Start server
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
